d5e1b1b23928d862ae4952b96fa990a90b3ddf1d
Isaque-Ias <gabriel.isaias.gomes.silva@gmail.com>
2026-04-27 11:38:13 -0300

    Capítulo 3 e 4

--- loja/loja/admin.py
+++ loja/loja/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 
 from .models import *
-admin.site.register(Fabricante)
+
+class FabricanteAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+
+class ProdutoAdmin(admin.ModelAdmin):
+    date_hierarchy = 'criado_em'
+    list_display = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    empty_value_display = 'Vazio'
+    msgPromocao = models.CharField(null=True, max_length=100, blank=True)
+    fields = ('Produto', 'destaque', 'promocao', 'msgPromocao', 'preco', 'categoria',)
+    search_fields = ('Produto',)
+    exclude = ('msgPromocao',)
+
+admin.site.register(Fabricante, FabricanteAdmin)
 admin.site.register(Categoria)
-admin.site.register(Produto)
+admin.site.register(Produto, ProdutoAdmin)
